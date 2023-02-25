c645766d37c0be3c429af55c9eb9dff7d69fe9fd
Hamna-Akh <hamnaakhter@hotmail.com>
2023-02-25 16:03:41 -0500

    testing abr buttons

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 let player = dashjs.MediaPlayer().create();
+var url = "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd";
 
-player.initialize(document.querySelector("#videoPlayer"), "https://nustreaming.github.io/streaming/bbb.mpd", true);
+player.initialize(document.querySelector("#videoPlayer"), url, true);
 player.on(dashjs.MediaPlayer.events["PLAYBACK_ENDED"], function () {
   clearInterval(eventPoller);
   clearInterval(bitrateCalculator);
 });
 
-player.updateSettings({
+        player.updateSettings({
             streaming: {
                 abr: {
                     useDefaultABRRules: true,
@@ -17,11 +18,134 @@ player.updateSettings({
                         droppedFramesRule: false,
                         abandonRequestsRule: false
                     }
-
                 }
             }
         });
 
+
+    // function disableABR(){
+    //     player.updateSettings({
+    //         streaming: {
+    //             abr: {
+    //                 useDefaultABRRules: false,
+    //                 additionalAbrRules: {
+    //                     insufficientBufferRule: true,
+    //                     switchHistoryRule: false,
+    //                     droppedFramesRule: false,
+    //                     abandonRequestsRule: false
+    //                 }
+    //             }
+    //         }
+    //     });
+    //     relaunchPlayer();
+    // }
+
+    // function enableABR(){
+    //     player.updateSettings({
+    //         streaming: {
+    //             abr: {
+    //                 useDefaultABRRules: true,
+    //                 additionalAbrRules: {
+    //                     insufficientBufferRule: true,
+    //                     switchHistoryRule: false,
+    //                     droppedFramesRule: false,
+    //                     abandonRequestsRule: false
+    //                 }
+    //             }
+    //         }
+    //     });
+    //     relaunchPlayer();
+    // }
+
+    // function enableDynamicABR(){
+    //     player.updateSettings({
+    //         streaming: {
+    //             abr: {
+    //                 useDefaultABRRules: true,
+    //                 ABRStrategy: 'abrDynamic',
+    //                 additionalAbrRules: {
+    //                     insufficientBufferRule: true,
+    //                     switchHistoryRule: false,
+    //                     droppedFramesRule: false,
+    //                     abandonRequestsRule: false
+    //                 }
+    //             }
+    //         }
+    //     });
+    //     relaunchPlayer();
+    // }
+
+    // function enableBOLATABR(){
+    //     player.updateSettings({
+    //         streaming: {
+    //             abr: {
+    //                 useDefaultABRRules: true,
+    //                 ABRStrategy: 'bola',
+    //                 additionalAbrRules: {
+    //                     insufficientBufferRule: true,
+    //                     switchHistoryRule: false,
+    //                     droppedFramesRule: false,
+    //                     abandonRequestsRule: false
+    //                 }
+    //             }
+    //         }
+    //     });
+    //     relaunchPlayer();
+    // }
+
+    // function enableThroughputABR(){
+    //     player.updateSettings({
+    //         streaming: {
+    //             abr: {
+    //                 useDefaultABRRules: true,
+    //                 ABRStrategy: 'throughput',
+    //                 additionalAbrRules: {
+    //                     insufficientBufferRule: true,
+    //                     switchHistoryRule: false,
+    //                     droppedFramesRule: false,
+    //                     abandonRequestsRule: false
+    //                 }
+    //             }
+    //         }
+    //     });
+    //     relaunchPlayer();
+    // }
+
+
+
+        function disableABR() {
+            player.updateSettings({'streaming': {'abr': {'useDefaultABRRules': false }}});
+            relaunchPlayer();
+        }
+
+        function enableABR() {
+            player.updateSettings({'streaming': {'abr': {'useDefaultABRRules': true }}});
+            relaunchPlayer();
+        }
+
+        function enableDynamicABR() {
+            player.updateSettings({'streaming': {'abr': {'ABRStrategy': 'abrDynamic' }}});
+            relaunchPlayer();
+        }
+
+        function enableBOLATABR() {
+            player.updateSettings({'streaming': {'abr': {'ABRStrategy': 'bola' }}});
+            relaunchPlayer();
+        }
+
+        function enableThroughputABR() {
+            player.updateSettings({'streaming': {'abr': {'ABRStrategy': 'throughput'}}});
+            relaunchPlayer();
+        }
+
+    function relaunchPlayer() {
+        player.reset();
+        player.initialize(document.querySelector("#videoPlayer"), url, true);
+        player.on(dashjs.MediaPlayer.events["PLAYBACK_ENDED"], function () {
+                clearInterval(eventPoller);
+                clearInterval(bitrateCalculator);
+        });
+    }
 
 
 var eventPoller = setInterval(function () {
@@ -56,7 +180,7 @@ var eventPoller = setInterval(function () {
       document.getElementById('segmentTime').innerText = "Segment Time: " + (segmentDownloadTime).toFixed(2) + " seconds";
       document.getElementById('segmentSize').innerText = "Segment Size: " + (segmentSize).toFixed(2) + " Bytes";
 
-
+      console.log("ABR strategy: " + player.getSettings().streaming.abr.ABRStrategy);
       console.log("buffer level: " + bufferLevel + " seconds")
       console.log("bitrate: " + (bitrate/1000).toFixed(2) + " Mbps")
       console.log("throughput: " + (throughput/1000000).toFixed(2) + " Mbps")
