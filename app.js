ea2d393ad9603df866ed44adcfb8651ac9a3da5b
Antonio Reda <70113532+AntoReda@users.noreply.github.com>
2023-02-25 17:00:14 -0500

    added bugfixes and comments in the code
    -What is missing: Extra CSS (optional)

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-let player = dashjs.MediaPlayer().create();
-var url = "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd";
+//Authors: @Hamna Akther and @Antonio Reda
+//Purpose: Create a DASH.js video player while allowing the switch of different types of ABR as well as recording and displaying the metrics.
 
+
+//Creates a DASH.js video player.
+let player = dashjs.MediaPlayer().create();
+//provides an mpd for the video player.
+var url = "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd";
+//initializes the video player and clears out unwanted noise from the metrics.
 player.initialize(document.querySelector("#videoPlayer"), url, true);
 player.on(dashjs.MediaPlayer.events["PLAYBACK_ENDED"], function () {
   clearInterval(eventPoller);
   clearInterval(bitrateCalculator);
 });
 
+//List of ABR types (Dynamic, BOLA, Throughput).
     function enableDynamicABR(){
         player.updateSettings({
             streaming: {
@@ -22,11 +29,11 @@ player.on(dashjs.MediaPlayer.events["PLAYBACK_ENDED"], function () {
                 }
             }
         });
-        
+        //reloads the video player.
         player.initialize(document.querySelector("#videoPlayer"), url, true);
     }
 
-    function enableBOLATABR(){
+    function enableBOLAABR(){
         player.updateSettings({
             streaming: {
                 abr: {
@@ -41,7 +48,7 @@ player.on(dashjs.MediaPlayer.events["PLAYBACK_ENDED"], function () {
                 }
             }
         });
-        
+        //reloads the video player.
         player.initialize(document.querySelector("#videoPlayer"), url, true);
     }
 
@@ -60,47 +67,52 @@ player.on(dashjs.MediaPlayer.events["PLAYBACK_ENDED"], function () {
                 }
             }
         });
-        
+        //reloads the video player.
         player.initialize(document.querySelector("#videoPlayer"), url, true);
     }
 
+//This function displays each of the metrics every 8 seconds.
 var eventPoller = setInterval(function () {
   var streamInfo = player.getActiveStream().getStreamInfo();
   var dashMetrics = player.getDashMetrics();
   var dashAdapter = player.getDashAdapter();
-
+//Ensures the video player calculates and records metrics.
   if (dashMetrics && streamInfo) {
       const periodIdx = streamInfo.index;
       var repSwitch = dashMetrics.getCurrentRepresentationSwitch('video', true);
+      //Retrieves the buffer level.
       var bufferLevel = dashMetrics.getCurrentBufferLevel('video', true);
-      var bitrate = repSwitch ? Math.round(dashAdapter.getBandwidthForRepresentation(repSwitch.to, periodIdx) / 1000) : NaN;
+      //Retrieves the bitrate.
+      var bitrate = repSwitch ? Math.round(dashAdapter.getBandwidthForRepresentation(repSwitch.to, periodIdx)) : NaN;
       var adaptation = dashAdapter.getAdaptationForType(periodIdx, 'video', streamInfo);
+      //Retrieves the throughput.
       var throughput = player.getAverageThroughput('video');
       var currentRep = adaptation.Representation_asArray.find(function (rep) {
           return rep.id === repSwitch.to
       })
 
-      // Retrieve the segment time
+      // Retrieve the Segment time
       var requestTime = dashMetrics.getCurrentHttpRequest('video', true).trequest;
       var responseTime = dashMetrics.getCurrentHttpRequest('video', true).tresponse;
       var segmentDownloadTime = responseTime - requestTime;
 
-      // Retrieve Segment size
+      // Retrieve the Segment size (in bytes)
       var segmentSize = (segmentDownloadTime * throughput)/8
 
-      var frameRate = currentRep.frameRate;
-      var resolution = currentRep.width + 'x' + currentRep.height;
-      document.getElementById('bufferLevel').innerText = "Buffer level: " + bufferLevel + " seconds";
-      document.getElementById('reportedBitrate').innerText = "Bitrate: " + (bitrate/1000000).toFixed(2) + " Mbps";
+      
+      //Displays the metrics on the page.
+      document.getElementById('bufferLevel').innerText = "Buffer level: " + bufferLevel + " Seconds";
+      document.getElementById('reportedBitrate').innerText = "Bitrate: " + (bitrate/1000000).toFixed(5) + " Mbps";
       document.getElementById('throughput').innerText = "Throughput: " + (throughput/1000000).toFixed(5) + " Mbps";
-      document.getElementById('segmentTime').innerText = "Segment Time: " + (segmentDownloadTime).toFixed(2) + " seconds";
-      document.getElementById('segmentSize').innerText = "Segment Size: " + (segmentSize).toFixed(2) + " Bytes";
+      document.getElementById('segmentTime').innerText = "Segment Time: " + (segmentDownloadTime).toFixed(5) + " Seconds";
+      document.getElementById('segmentSize').innerText = "Segment Size: " + (segmentSize).toFixed(5) + " Bytes";
 
+      //Displays the metrics in the console.
       console.log("ABR strategy: " + player.getSettings().streaming.abr.ABRStrategy);
-      console.log("buffer level: " + bufferLevel + " seconds")
-      console.log("bitrate: " + (bitrate/1000000).toFixed(2) + " Mbps")
+      console.log("buffer level: " + bufferLevel + " Seconds")
+      console.log("bitrate: " + (bitrate/1000000).toFixed(5) + " Mbps")
       console.log("throughput: " + (throughput/1000000).toFixed(5) + " Mbps")
-      console.log("Segment Time: " + (segmentDownloadTime).toFixed(2) + " seconds")
+      console.log("Segment Time: " + (segmentDownloadTime).toFixed(5) + " Seconds")
       console.log("Segment Size: " + (segmentSize).toFixed(2) + " Bytes")
       console.log(" ")
   }
